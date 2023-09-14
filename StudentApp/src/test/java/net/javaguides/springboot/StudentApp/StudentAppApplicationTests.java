299f7e9db78324547b7d8741993abf238b281b11
Helen Charbonnet <helen.charbonnet@afs.com>
2023-09-14 14:26:24 -0400

    initial project setup

---
+++ StudentApp/src/test/java/net/javaguides/springboot/StudentApp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.StudentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
